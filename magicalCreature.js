b01b597b46f5126bc4f06a84eafeca3992381698
xQingChen <139983168+xQingChen@users.noreply.github.com>
2023-10-18 22:20:34 +0000

    Nice!!!

--- magicalCreature.js
+++ magicalCreature.js
@@ -2,35 +2,39 @@
 
 // 1. Declare a variable named 'creatureName' and assign it a magical creature's name as a string.
 
-
+let creatureName = "dragon";
 
 // 2. Declare a variable named 'creatureDescription' and describe its magical properties as a string.
 
+let creatureDescription = "The dragon has fiery wings and releases burning fire balls when you anger it!";
 
 // 3. Do you believe that this creature is friendly to humans? 
 // Declare a variable named 'isFriendly' and assign it a boolean value.
 
-
+let isFriendly = true;
 
 // 4. The magical zoo has an age restriction! Declare a variable named 'visitorAge' and assign it a random age between 1 and 60.
 // Hint: Use Math.floor() to round a number up.
 // Hint: Use Math.random() for generating random numbers.
 
-
+let visitorAge = Math.floor(Math.random() * 60); 
 
 // 5. Determine if the visitor is allowed to see the creature.
 // Declare a variable named 'canVisit' and assign it an expression to see 
 // if the visitorAge variable is greater than 15.
 // Hint: use this resource if you need support: https://www.w3schools.com/js/js_booleans.asp 
 
+let canVisit = visitorAge > 15;
 
 
 // 6. Combine all the information into a summary.
 // Declare a variable named 'zooSummary' and combine all the above information using concatenation.
 
+let zooSummary = "The " + creatureName + " is " + creatureDescription + ". It is " + isFriendly + "that it is friendly."
 
 // 7. Print the summary using console.log()
 
+console.log(zooSummary);
 
 
 // Challenge:
--- operators.js
+++ operators.js
@@ -6,18 +6,24 @@
 // Add the two numbers and store the result in a new variable called sum.
 // Print the result using console.log().
 
-
+let num1 = 10;
+let num2 = 25;
+let sumOfNum1andNum2 = num1 + num2; 
+console.log(sumOfNum1andNum2);
 
 // 2. Subtraction
 // Subtract num2 from num1 and store the result in a new variable called difference.
 // Print the result.
 
+let difference = num2 - num1; 
+console.log(difference);
 
 // 3. Multiplication
 // Multiply num1 by num2 and store the result in a new variable called product.
 // Print the result.
 
-
+let product = num1 * num2;
+console.log(product);
 
 // String Operations
 
@@ -25,4 +31,9 @@
 // Create two string variables, str1 and str2, and assign them any two strings of your choice.
 // Concatenate (combine) the two strings using the + operator and store the result in a new variable called combinedString.
 // Print the result.
+
+let str1 = "Hello";
+let str2 = "World";
+let combinedString = str1 + str2;
+console.log(combinedString);
 
